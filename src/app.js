8b457298f995b32f3f676f792da39b3e26638288
Benjamin <catiremedina@gmail.com>
2021-11-08 16:56:47 -0400

    proyect done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusometro = () => {
+    let quien = ["Mi perro", "El gato", "Mi mama", "Aquel"];
+    let que = ["se comio", "se robo", "escondio", "rompio", "batqueo"];
+    let aque = ["la tarea", "el computador", "mi almuerzo", "los brownie"];
+    let cuando = ["hoy", "ayer", "antielsito", "anoche", "ahorita mesmo"];
+
+    let quieni = Math.floor(Math.random() * 4);
+    let quei = Math.floor(Math.random() * 4);
+    let aquei = Math.floor(Math.random() * 4);
+    let cuandoi = Math.floor(Math.random() * 4);
+
+    return (
+      quien[quieni] +
+      " " +
+      que[quei] +
+      " " +
+      aque[aquei] +
+      " " +
+      cuando[cuandoi]
+    );
+  };
+  document.getElementById("eboton").addEventListener("click", () => {
+    document.getElementById("excusa").innerHTML = excusometro();
+  });
 };
